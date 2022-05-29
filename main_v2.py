233cea6688f9c2e36c620a4bae96386aa95a2efe
marcuslowhuiyu <marcuslowhuiyu@gmail.com>
2022-05-30 00:50:22 +0800

    Update main_v2.py

--- main_v2.py
+++ main_v2.py
@@ -1,9 +1,12 @@
 import telebot
 import logging 
+from telebot import types
 
 logger = telebot.logger
 telebot.logger.setLevel(logging.DEBUG)
 bot = telebot.TeleBot("5376242962:AAGxLOy-Yd8MMYvoxBft_7wULmL-GB2eFcM", parse_mode = None)
+
+payees = {}
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
@@ -13,39 +16,25 @@ def send_welcome(message):
 def send_help(message):
     bot.reply_to(message, f"/createorder: start a new order")
 
-curr_order_options = []
-curr_order = []
-
 @bot.message_handler(commands=["createorder"])
 def create_order(message):
-    bot.reply_to(message, f"new order! /add order, /completeorder when done")
-    global curr_order_options
-    curr_order_options = []
+    bot.reply_to(message, f"/add Followed by your order \n/completeorder When done")
 
 @bot.message_handler(commands=["add"])
 def add_option(message):
-    global curr_order_options
     option = message.text[5:]
-    if option in curr_order_options:
-        bot.reply_to(message, f"{option} already exists!")
-        return
-    curr_order_options.append(option)
+    markup = types.ForceReply(selective=True)
+    bot.send_message(message, "Who made this order?", reply_markup=markup)
+    payee = message.text
+    if payee in payees:
+        payees[payee].append(option)
+    else: 
+        payees[payee] = [option]
     bot.reply_to(message, f"{option} added!")
 
 @bot.message_handler(commands=["completeorder"])
 def complete_order(message):
-    global curr_order, curr_order_options
-    if curr_order and not curr_order[0].is_closed:
-        bot.reply_to(message, f"one order at a time!")
-        return
-    options = curr_order_options
-    order = bot.send_poll(
-        message.chat.id, 
-        "Who order What?", 
-        options,
-        allows_multiple_answers=True
-    )
-    curr_order = [order]
+    pass
 
 @bot.message_handler(func = lambda m: True)
 def echo_all(message):
