6b181ce802083fbc3f83f05da0099e85350432df
mlzt2000 <mlzt2000@gmail.com>
2022-06-03 18:37:02 +0800

    Modified complete_order() to use original API

--- main.py
+++ main.py
@@ -30,8 +30,23 @@ def create_order(update: Update, context: CallbackContext) -> None:
 def add_option(update: Update, context: CallbackContext) -> None:
     text = update.message.text.split(" ")[1:]
     name, purchase, cost = text[0], text[1], text[2]
-    msg = f"{name} buys {purchase} for ${cost}"
+    msg = f"Order added! {name} buys {purchase} for ${cost}"
     update.message.reply_text(f"{msg}\n\n/vieworder --> Views all options in the current order\n\n/completeorder --> Sends the current order as a poll into the chat and resets the order")
+
+def view_order(update: Update, context: CallbackContext) -> None:
+    msg = str.join(orders, "\n")
+    update.message.reply_text(f"{msg}")
+
+def complete_order(update: Update, context: CallbackContext) -> None:
+    update_chat_id = update.message.chat.id
+    context.bot.send_poll(
+        chat_id = update_chat_id,
+        question = f"Who order what",
+        options = orders,
+        is_anonymous = False,
+        allows_multiple_answers = True
+    )
+
 
 
     
@@ -54,62 +69,3 @@ def main():
 
 if __name__ == "main":
     main()
-
-import telebot
-from telebot import types
-
-logger = telebot.logger
-telebot.logger.setLevel(logging.DEBUG)
-bot = telebot.TeleBot("5457184587:AAE5SOisTmph4cvKrYPw1k33Rpx-NwW6BLA", parse_mode = None)
-
-orders = []
-
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, f"Thank you for using PayLiaoBot!\nCreate a new order with /createorder, or view all commands with /help!")
-
-@bot.message_handler(commands=['help'])
-def send_help(message):
-    bot.reply_to(message, f"/createorder: start a new order")
-
-@bot.message_handler(commands=["createorder"])
-def create_order(message):
-    bot.reply_to(message, f"/add Followed by your order in this format: OrderName NameOfPerson Cost \n/vieworder When done")
-
-@bot.message_handler(commands=["add"])
-def add_option(message):
-    msg = message.text[5:].split(" ")
-    option = msg[0]
-    payee = msg[1]
-    cost = msg[2]
-    msg = f"{payee} owes {cost} for {option}"
-    orders.append(msg)
-    bot.reply_to(message, f"Order added!")
-    bot.reply_to(message, msg)
-    bot.reply_to(message, f"/add Followed by your order in this format: OrderName NameOfPerson Cost \n/vieworder When done")
-
-@bot.message_handler(commands=["vieworder"])
-def view_order(message):
-    final = ""
-    for order in orders:
-        final += f"{order}\n"
-    bot.reply_to(message, final)
-    bot.reply_to(message, f"/completeorder to send the poll!")
-
-@bot.message_handler(commands=["completeorder"])
-def complete_order(message):
-    bot.send_poll(
-        chat_id = message.chat.id,
-        question = "Have you paid?",
-        options = orders,
-        allows_multiple_answers=False,
-        is_anonymous=False,
-    )
-    orders.clear()
-    
-
-# @bot.message_handler(func = lambda m: True)
-# def echo_all(message):
-#     bot.reply_to(message, message.text)
-
-bot.infinity_polling()
